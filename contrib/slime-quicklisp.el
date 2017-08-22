c97077abfbf192938257f08d2914b8f6d7a61449
Luís Oliveira <loliveira@common-lisp.net>
2017-08-23 00:31:12 +0100

    More signature verifications and hand holding

--- contrib/slime-quicklisp.el
+++ contrib/slime-quicklisp.el
@@ -1,5 +1,6 @@
 (require 'slime)
 (require 'cl-lib)
+(eval-when-compile (require 'epg))
 
 ;;; bits of the following taken from slime-asdf.el
 
@@ -43,18 +44,64 @@
 (defvar slime-quicklisp-quickstart-url
   "https://beta.quicklisp.org/quicklisp.lisp")
 
-(defun slime-quicklisp-install ()
-  (interactive)
-  (let ((buffer (url-retrieve-synchronously slime-quicklisp-quickstart-url)))
+(defvar slime-quicklisp-quickstart-signature-url
+  "https://beta.quicklisp.org/quicklisp.lisp.asc")
+
+(defun slime-quicklisp--get (url-string)
+  (message "Downloading %s..." url-string)
+  (slime-eval `(cl:format cl:t "~&Downloading ~a...~%" ,url-string))
+  (let* ((url (url-generic-parse-url url-string))
+         (buffer (url-retrieve-synchronously url)))
     (with-current-buffer buffer
       (delete-region (point-min)
-                     (marker-position url-http-end-of-headers))
+                     (1+ (marker-position url-http-end-of-headers)))
+      ;; Ensure LF line endings for SBCL's and GnuPG's sanity.
       (set-buffer-file-coding-system 'unix)
-      (lisp-mode)
-      (let ((file (make-temp-file "quicklisp.lisp.")))
-        (write-file file)
-        (slime-load-file file)
-        (kill-buffer)))))
+      (let* ((name (file-name-nondirectory (url-filename url)))
+             (file (make-temp-file (concat (file-name-sans-extension name) "-")
+                                   nil
+                                   (concat "." (file-name-extension name)))))
+        (let ((inhibit-message t))
+          (write-file file))
+        (kill-buffer)
+        file))))
+
+(defun slime-quicklisp--verify (file signature-file)
+  (let* ((context (epg-make-context)))
+    (epg-verify-file context signature-file file)
+    ;; assuming there's only one signature.
+    (let ((sig (first (epg-context-result-for context 'verify))))
+      (check-type sig epg-signature)
+      (slime-eval `(cl:format cl:t "~&~a.~%" ,(epg-signature-to-string sig)))
+      (or (eq (epg-signature-status sig) 'good)
+          (yes-or-no-p (concat (epg-signature-to-string sig)
+                               ".\n\nFailed to verify Quicklisp signature. \
+                                Continue anyway? "))))))
+
+(defun slime-quicklisp--suggest-add-to-init-file (result)
+  (declare (ignore result))
+  (when (yes-or-no-p "Add Quicklisp to init file? ")
+    (slime-eval-async `(ql-util:without-prompting
+                         (ql:add-to-init-file)
+                         (cl:values)))))
+
+(defun slime-quicklisp--suggest-install (success)
+  (when (and success
+             (yes-or-no-p "Proceed with quicklisp-quickstart:install? "))
+    (slime-eval-async '(quicklisp-quickstart:install)
+                      #'slime-quicklisp--suggest-add-to-init-file)))
+
+(defun slime-quicklisp-install ()
+  "Install the Quicklisp client."
+  (interactive)
+  (require 'epg)
+  (slime-display-output-buffer)
+  (slime-eval `(cl:progn (cl:terpri) (cl:terpri)))
+  (let ((quickstart (slime-quicklisp--get slime-quicklisp-quickstart-url))
+        (signature (slime-quicklisp--get slime-quicklisp-quickstart-signature-url)))
+    (when (slime-quicklisp--verify quickstart signature)
+      (slime-eval-async `(cl:load ,quickstart)
+                        #'slime-quicklisp--suggest-install))))
 
 ;;; REPL shortcuts
 
